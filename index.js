4a9b625dea7a639f1f50d119be743f8236e5d912
datta-toma <tomadatta15@gmail.com>
2024-05-12 17:46:36 +0600

    booking date update

--- index.js
+++ index.js
@@ -86,6 +86,22 @@ async function run() {
     const result = await bookingCollection.deleteOne(query);
     res.send(result);
   })
+    
+
+  // update
+app.patch('/bookings/:id', async(req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedBooking = req.body; 
+      const updateDoc = {
+          $set: {
+            date: updatedBooking.date,
+          }
+      };
+
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+});
 
 
     
