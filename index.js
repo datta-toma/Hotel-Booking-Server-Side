79b6bb2da0da496a9f33d87ebc594529520316a6
datta-toma <tomadatta15@gmail.com>
2024-05-14 03:45:48 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 console.log(process.env.DB_PASS)
@@ -25,6 +31,36 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+ // middlewares
+ const logger = async(req, res, next) =>{
+  console.log('called', req.method, req.url)
+  next();
+}
+
+const verifyToken = async(req, res, next) =>{
+  const token = req?.cookies?.token;
+  console.log('value of token', token)
+  if(!token){
+    return res.status(401).send({message: 'not authorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    //  error
+    if(err){
+      console.log(err);
+      return res.status(401).send({message: 'unauthorized'})
+    }
+
+    // if token is valid then it would be decoded
+    console.log('value in the token', decoded)
+    req.user = decoded;
+    next()
+  })
+
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -34,8 +70,29 @@ async function run() {
     const bookingCollection = client.db('hotelRoom').collection('booking');
     const reviewCollection = client.db('hotelRoom').collection('reviews');
 
+    // auth related api
+    app.post('/jwt', logger, async(req, res)=>{    
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        
+      })
+      .send({success: true})
+    })
 
-    app.get('/Rooms', async(req, res)=>{
+    // logout
+    app.post('logout', async(req, res) =>{
+      const user = req.body;
+      console.log('login out', user);
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
+    })
+
+    //  hotel related api
+    app.get('/Rooms', logger, async(req, res)=>{
         const cursor = RoomsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
@@ -64,8 +121,12 @@ async function run() {
 
 // bookings
 
-  app.get('/bookings', async(req, res)=>{
+  app.get('/bookings', logger, verifyToken, async(req, res)=>{
     console.log(req.query.email);
+    console.log('token page', req.cookies.token)
+    if(req.user.email !== req.query.email){
+      return res.status(403).send({message: 'forbidden access'})
+    }
     let query = {};
     if(req.query?.email){
       query = {email: req.query.email}
@@ -106,7 +167,7 @@ app.patch('/bookings/:id', async(req, res) => {
       res.send(result);
 });
 
-
+// review
 app.get('/reviews', async(req, res)=>{
   const cursor = reviewCollection.find();
   const result = await cursor.toArray();
@@ -121,6 +182,7 @@ app.get('/reviews/:id', async(req, res)=>{
 })
 
 
+
     // POST route to save reviews
     app.post('/reviews', async (req, res) => {
       const { review } = req.body;
@@ -130,11 +192,7 @@ app.get('/reviews/:id', async(req, res)=>{
 
  
 
-    
-
-
-
-
+  
 
 
     // Send a ping to confirm a successful connection
